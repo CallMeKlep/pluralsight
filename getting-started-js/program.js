f3a0d690707d76acb07caa0885d56ea446e2415c
CallMeKlep <klepinger.s@gmail.com>
2015-03-07 03:28:35 -0500

    program to find an avg

--- getting-started-js/program.js
+++ getting-started-js/program.js
@@ -1,2 +1,4 @@
-lunch - "fish and chips"
-console.log(lunch)
+var totalCost = 14.99;
+var sizeOfParty = process.argv[2];
+var averageCost = totalCost / sizeOfParty;
+console.log("Your lunch is: $" + averageCost + " a piece, you cheap fuckers.");
