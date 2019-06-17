9125abc95d29cf4559252c7b4af754a5a3d79440
samk <samk@pqsystems.com>
2019-06-17 16:13:17 -0400

    Added constructor to intialize grades field

--- csharp_fundamentals_01/csharp_fundamentals_01/Book.cs
+++ csharp_fundamentals_01/csharp_fundamentals_01/Book.cs
@@ -6,10 +6,15 @@ namespace csharp_fundamentals_01
 {
     class Book
     {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
-        List<double> grades = new List<double>();
+        List<double> grades;
     }
 }
