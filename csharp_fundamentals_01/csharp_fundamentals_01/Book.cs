b5894ebab7a46dd1da0f297e19a6c8727a39c54d
samk <samk@pqsystems.com>
2019-06-17 16:04:03 -0400

    Added new 'Book' class

---
+++ csharp_fundamentals_01/csharp_fundamentals_01/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_fundamentals_01
+{
+    class Book
+    {
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        List<double> grades = new List<double>();
+    }
+}
--- csharp_fundamentals_01/csharp_fundamentals_01/Program.cs
+++ csharp_fundamentals_01/csharp_fundamentals_01/Program.cs
@@ -7,7 +7,10 @@ namespace csharp_fundamentals_01
     {
         static void Main(string[] args)
         {
-            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            var book = new Book();
+            book.AddGrade(89.1);
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
             var result = 0.0;
             foreach(var number in grades)
@@ -15,7 +18,7 @@ namespace csharp_fundamentals_01
                 result += number;
             }
             result /= grades.Count;
-            Console.WriteLine($"The average grade is {result}");
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
