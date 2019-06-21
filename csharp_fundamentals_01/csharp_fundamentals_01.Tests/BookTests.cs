03e7fdf2b3313c667b12deac8172b9fe1d612f0d
samk <klepinger.s@gmail.com>
2019-06-21 15:48:13 -0400

    Fixed typo. Renamed.

--- csharp_fundamentals_01/csharp_fundamentals_01.Tests/BookTests.cs
+++ csharp_fundamentals_01/csharp_fundamentals_01.Tests/BookTests.cs
@@ -6,13 +6,13 @@ namespace csharp_fundamentals_01.Tests
     public class BookTests
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAverageGrade()
         {
             // arrange
             var book = new Book("");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            book.AddGrade(77.3);
+            book.AddGrade(77.5);
 
             // act
             var result = book.GetStatistics();
@@ -20,7 +20,7 @@ namespace csharp_fundamentals_01.Tests
             // assert
             Assert.Equal(85.6, result.Average, 1);
             Assert.Equal(90.5, result.High);
-            Assert.Equal(77.3, result.Low);
+            Assert.Equal(77.5, result.Low);
         }
     }
 }
