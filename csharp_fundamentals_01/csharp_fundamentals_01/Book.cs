e07edb5bdb78bb570d3568afb643acdcbc97c140
samk <klepinger.s@gmail.com>
2019-06-25 11:59:12 -0400

    for loop example
    Note: Recommends using a foreach loop if possible/appropriate

--- csharp_fundamentals_01/csharp_fundamentals_01/Book.cs
+++ csharp_fundamentals_01/csharp_fundamentals_01/Book.cs
@@ -27,14 +27,12 @@ namespace csharp_fundamentals_01
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            var index = 0;
-            while (index < grades.Count)
+            for(var index = 0; index < grades.Count; index += 1)
             {
                 result.High = Math.Max(grades[index], result.High);
                 result.Low = Math.Min(grades[index], result.Low);
                 result.Average += grades[index];
-                index += 1;
-            };
+            }
             result.Average /= grades.Count;
             return result;
         }
