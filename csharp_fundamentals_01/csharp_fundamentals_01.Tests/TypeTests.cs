94889eadd859ff46b8fe78509a6e6a2d7aead97a
samk <klepinger.s@gmail.com>
2019-06-25 10:24:51 -0400

    Reference vs value type examples

--- csharp_fundamentals_01/csharp_fundamentals_01.Tests/TypeTests.cs
+++ csharp_fundamentals_01/csharp_fundamentals_01.Tests/TypeTests.cs
@@ -6,13 +6,45 @@ namespace csharp_fundamentals_01.Tests
     public class TypeTests
     {
         [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int z)
+        {
+            z = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
         public void CSharpIsPassByValue()
         {
             var book1 = GetBook("Book 1");
             GetBookSetName(book1, "New Name");
 
             Assert.Equal("Book 1", book1.Name);
-
         }
 
         private void GetBookSetName(Book book, string name)
@@ -27,12 +59,23 @@ namespace csharp_fundamentals_01.Tests
             SetName(book1, "New Name");
 
             Assert.Equal("New Name", book1.Name);
-
         }
 
         private void SetName(Book book, string name)
         {
             book.Name = name;
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Sam";
+            MakeUppercase(name);
+        }
+
+        private void MakeUppercase(string parameter)
+        {
+            parameter.ToUpper();
         }
 
         [Fact]
