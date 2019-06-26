d76f44135bdb799b9ed6e56043c464bfa7c70026
samk <klepinger.s@gmail.com>
2019-06-26 13:15:22 -0400

    Removed example

--- python_introduction/guess.py
+++ python_introduction/guess.py
@@ -1,10 +1,5 @@
 import random
 
-print("Hello, what is your favorite number?")
-number = input()
-
-print("Your favorite number is " + number)
-
 minNumber = 1
 maxNumber = 100
 magicNumber = random.randint(minNumber,maxNumber)
