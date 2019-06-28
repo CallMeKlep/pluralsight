59e501fdc0f9a4ef1ce7f5b7ba8fd00eebbc0452
samk <klepinger.s@gmail.com>
2019-06-28 13:48:15 -0400

    Polymorphism example

--- csharp_fundamentals_01/csharp_fundamentals_01/Book.cs
+++ csharp_fundamentals_01/csharp_fundamentals_01/Book.cs
@@ -6,13 +6,29 @@ namespace csharp_fundamentals_01
 {
     public delegate void GradeAddedDelegate(object sender, EventArgs args);
 
-    public abstract class Book : NamedObject
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+
+    public abstract class Book : NamedObject, IBook
     {
         public Book(string name) : base(name)
         {
 
         }
+
+        public virtual event GradeAddedDelegate GradeAdded;
+
         public abstract void AddGrade(double grade);
+
+        public virtual Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class InMemoryBook : Book
@@ -57,7 +73,7 @@ namespace csharp_fundamentals_01
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
-        public Statistics GetStatistics()
+        public override Statistics GetStatistics()
         {
             var result = new Statistics();
             result.Average = 0.0;
@@ -94,7 +110,7 @@ namespace csharp_fundamentals_01
             return result;
         }
 
-        public event GradeAddedDelegate GradeAdded;
+        public override event GradeAddedDelegate GradeAdded;
 
         private List<double> grades;
 
--- csharp_fundamentals_01/csharp_fundamentals_01/Program.cs
+++ csharp_fundamentals_01/csharp_fundamentals_01/Program.cs
@@ -22,7 +22,7 @@ namespace csharp_fundamentals_01
             Console.WriteLine($"The letter grade is {stats.Letter}");
         }
 
-        private static void EnterGrades(Book book)
+        private static void EnterGrades(IBook book)
         {
             while (true)
             {
